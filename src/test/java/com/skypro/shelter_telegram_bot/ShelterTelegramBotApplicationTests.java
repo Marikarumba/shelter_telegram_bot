af83ce228f8af377d5814db1ef8ca41831d4c8ba
Marika <marikarumba@mail.ru>
2023-02-02 22:16:50 +0300

    Initial commit

---
+++ src/test/java/com/skypro/shelter_telegram_bot/ShelterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.shelter_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
