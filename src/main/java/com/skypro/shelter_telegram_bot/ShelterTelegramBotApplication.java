012332906537f6734dc7097be01cf86bdc2ec687
Marika <marikarumba@mail.ru>
2023-02-02 22:16:50 +0300

    Initial commit

---
+++ src/main/java/com/skypro/shelter_telegram_bot/ShelterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.shelter_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterTelegramBotApplication.class, args);
+    }
+
+}
