a54a197202bc17d6dbea90f642d9b0720c2f84a3
Marika <marikarumba@mail.ru>
2023-02-02 22:51:22 +0300

    Start menu and buttons ready

---
+++ src/main/java/com/skypro/shelter_telegram_bot/InlineKeyboardMaker.java
@@ -0,0 +1,37 @@
+package com.skypro.shelter_telegram_bot;
+
+import com.skypro.shelter_telegram_bot.constants.StartMenuResourceEnum;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class InlineKeyboardMaker {
+
+    public InlineKeyboardMarkup getInlineMessageButtons() {
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+        for (StartMenuResourceEnum startMenuResourceEnum : StartMenuResourceEnum.values()) {
+            rowList.add(getButton(
+                    startMenuResourceEnum.getButtonName(),
+                    startMenuResourceEnum.getCommand()));
+        }
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        inlineKeyboardMarkup.setKeyboard(rowList);
+
+        return inlineKeyboardMarkup;
+    }
+
+    private List<InlineKeyboardButton> getButton(String buttonName, String command) {
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText(buttonName);
+        button.setCallbackData(command);
+
+        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+        keyboardButtonsRow.add(button);
+        return keyboardButtonsRow;
+    }
+}
