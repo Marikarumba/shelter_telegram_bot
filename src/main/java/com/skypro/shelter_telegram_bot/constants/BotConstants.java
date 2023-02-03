0fbdcfebc73ec8100404caf644802c655c85ceca
ams32ru <ams32ru@mail.ru>
2023-02-03 22:13:36 +0300

    Глвное меню и меню инфо

--- src/main/java/com/skypro/shelter_telegram_bot/constants/BotConstants.java
+++ src/main/java/com/skypro/shelter_telegram_bot/constants/BotConstants.java
@@ -2,6 +2,10 @@ package com.skypro.shelter_telegram_bot.constants;
 
 public class BotConstants {
 
+    public static final String INITIAL_CMD = "/start";
+    public static final String FINAL_CMD = "/end";
+    public static final String INFO_SHELTER_CMD = "/info";
+
 
     public static final String GREETING_MSG = """
              Привет!
