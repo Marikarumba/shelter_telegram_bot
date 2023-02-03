717011ae3da017415c794eda3c9f53ae87c650f8
ams32ru <ams32ru@mail.ru>
2023-02-04 01:56:50 +0300

    доработал меню инфо

--- src/main/java/com/skypro/shelter_telegram_bot/BottomMenu/StartMenuResourceEnum.java
+++ src/main/java/com/skypro/shelter_telegram_bot/BottomMenu/StartMenuResourceEnum.java
@@ -1,18 +1,20 @@
-package com.skypro.shelter_telegram_bot.constants;
+package com.skypro.shelter_telegram_bot.BottomMenu;
 
 import lombok.Getter;
-
-import static com.skypro.shelter_telegram_bot.constants.BotConstants.FINAL_CMD;
-
+import static com.skypro.shelter_telegram_bot.constants.BotConstants.*;
 
 public enum StartMenuResourceEnum {
 
-    STEP_1("K1", "Информация о приюте"),
+    /*
+    Главное меню
+     */
+
+    STEP_1( INFO_SHELTER_CMD,"Информация о приюте"),
     STEP_2("K2", "Как взять животное"),
     STEP_3("K3", "Прислать отчет"),
-    STEP_4("К4", "Позвать волонтера"),
-    STEP_5(FINAL_CMD, "Заказать обратный звонок");
-
+    STEP_4(CALL_VOLUNTEER_CMD, "Позвать волонтера"),
+    STEP_5(CALL_BACK_CMD, "Заказать обратный звонок");
+    @Getter
     private final String command;
     @Getter
     private final String buttonName;
